20bea8536bca1665702fb03c8bdc8176c575e7cc
Meyer <bedwars0440@gmail.com>
2023-05-04 00:21:25 +0000

    multi line working?

--- exts/caption.py
+++ exts/caption.py
@@ -41,9 +41,22 @@ class caption(commands.Cog):
             print(e)
             return 1
 
-    async def gen_text(self, caption: str, filename: str, ctx, f_size: int = 24) -> int:
+    async def gen_text(self, caption: str, filename: str, ctx, f_size: int = 24) -> list[int, int, int]:
         from PIL import Image, ImageDraw, ImageFont
         try:
+            #funtions and shit
+            def get_words_per_line(bg_w, tmp_cpn, draw, font_s:int=50 ):
+                for i in reversed(range(len(tmp_cpn))):
+                    font = ImageFont.truetype('./Futura-Condensed-Bold.ttf', 50)
+                    cpn= ' '.join(tmp_cpn[0:i-1])
+                    tmp_text_width, tmp_text_height = draw.textsize(cpn, font=font) 
+                    if tmp_text_width< bg_w:
+                        return (i-1, tmp_text_height, len(tmp_cpn)//i-1, font_s) #returns (words per line, height of words(max), total lines,font size(will be used soon:tm:)  )
+                    #minus one to account for differnt length words
+                    # might change to -2 
+            
+            #-------------------------------------------------------------------------------------------------------------------------------------
+            
             ogimg = Image.open(f'./images/{filename}')
             # origonial height and width of image to be captioned
             (ogw, ogh) = ogimg.size
@@ -65,27 +78,38 @@ class caption(commands.Cog):
             lines=1
             text_width, text_height = draw.textsize(caption, font=font)
             if text_width >= bg_w:
-                tmp_cpn=caption.split(' ')
-                split=len(tmp_cpn)//2
-                cpn_1=' '.join(tmp_cpn[0:split])
-                print(cpn_1)
-                cpn_2=' '.join(tmp_cpn[split:])
-                print(cpn_2)
-                lines=2
-            if lines==2:
-                text_width1, text_height1 = draw.textsize(cpn_1, font=font)
-                text_y1 = ((bg_h - text_height1) // 3)
-                text_x1 = (bg_w - text_width1) // 2
-                text_width2, text_height2 = draw.textsize(cpn_2, font=font)
-                text_y2 = ((bg_h - text_height2) // 3)*2
-                text_x2 = (bg_w - text_width2) // 2
-                draw.text((text_x1, text_y1), cpn_1, font=font, fill=txt_color)
-                draw.text((text_x2, text_y2), cpn_2, font=font, fill=txt_color)
-            if lines==1:
+                tmp_cpn=' '.join(caption)
+                linedata=get_words_per_line(bg_w, tmp_cpn, draw)
+                bg = Image.new('RGB', (bg_w, (linedata[1]*(linedata[2]+2))), bg_color)
+                bg_w, bg_h=bg.size
+                font = ImageFont.truetype('./Futura-Condensed-Bold.ttf',50)
+                draw=ImageDraw.Draw(bg)
+                
+                for i in range(linedata[2]+1):
+                    tmp_text_width, tmp_text_height = draw.textsize(tmp_cpn[i*linedata[0]:(i+1)*linedata[0]], font=font)
+                    text_x = (bg_w - tmp_text_width ) // 2 
+                    text_y=((bg_h-linedata[1])//linedata[2])*(i+1)
+                    draw.text((text_x, text_y),tmp_cpn[i*linedata[0]:(i+1)*linedata[0]],font=font, fill=txt_color)
+            #     split=len(tmp_cpn)//2
+            #     cpn_1=' '.join(tmp_cpn[0:split])
+            #     print(cpn_1)
+            #     cpn_2=' '.join(tmp_cpn[split:])
+            #     print(cpn_2)
+            #     lines=2
+            # if lines==2:
+            #     text_width1, text_height1 = draw.textsize(cpn_1, font=font)
+            #     text_y1 = ((bg_h - text_height1) // 3)
+            #     text_x1 = (bg_w - text_width1) // 2
+            #     text_width2, text_height2 = draw.textsize(cpn_2, font=font)
+            #     text_y2 = ((bg_h - text_height2) // 3)*2
+            #     text_x2 = (bg_w - text_width2) // 2
+            #     draw.text((text_x1, text_y1), cpn_1, font=font, fill=txt_color)
+            #     draw.text((text_x2, text_y2), cpn_2, font=font, fill=txt_color)
+            # if lines==1:
 
-                text_x = (bg_w - text_width) // 2
-                text_y = (bg_h - text_height) // 2
-                draw.text((text_x, text_y), caption, font=font, fill=txt_color, anchor='mm')
+            #     text_x = (bg_w - text_width) // 2
+            #     text_y = (bg_h - text_height) // 2
+            #     draw.text((text_x, text_y), caption, font=font, fill=txt_color, anchor='mm')
                 
                 
 
---
+++ todo.md
@@ -0,0 +1,4 @@
+* using the length of a word, determin how many can fit on a line
+* divide words per line -2(to account for long words) by total words to find out total lines
+* if total lines is greater than 5 lower font size and try agian
+* set bottom limit for font size
